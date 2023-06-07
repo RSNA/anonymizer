21ddeb38197865af2e5a185b08bcf583448be135
mdevans <michael@dx.life>
2023-06-07 14:47:38 +0200

    About working

--- dicom_scrub.py
+++ dicom_scrub.py
@@ -1,12 +1,25 @@
 import customtkinter as ctk
 from PIL import Image
 
+ABOUT_WELCOME = "RSNA is assembling a repository of de-identified COVID-19-related imaging data for research and education. Medical institutions interested in joining this international collaboration are invited to submit de-identified imaging data for inclusion in this repository. The RSNA Anonymizer program is a free open-source tool for collecting and de-identifying DICOM studies to prepare them for submission. It may be used to ensure privacy by removing protected health information from your DICOM imaging studies.\n\nYou can learn more about the RSNA COVID-19 Imaging Data Repository initiative here."
+
 
 class TabViewNested(ctk.CTkTabview):
     def __init__(self, master, tabs, **kwargs):
         super().__init__(master, **kwargs)
         for tab in tabs:
-            self.add(tab)
+            tabview = self.add(tab)
+            tabview.rowconfigure(1, weight=1)
+            tabview.columnconfigure(0, weight=1)
+
+            if tab == "Welcome":
+                label_welcome = ctk.CTkLabel(
+                    master=tabview, text=tab, font=("Domine", 20)
+                )
+                label_welcome.grid(row=0, column=0, sticky="n")
+                text_box_welcome = ctk.CTkTextbox(tabview, wrap="word")
+                text_box_welcome.insert("0.0", text=ABOUT_WELCOME)
+                text_box_welcome.grid(row=1, column=0, sticky="nwe")
 
 
 class TabViewMain(ctk.CTkTabview):
@@ -29,12 +42,21 @@ class TabViewMain(ctk.CTkTabview):
 
 
 class App(ctk.CTk):
-    def __init__(self, title, logo_file, logo_width, logo_height, tabs, pad):
+    def __init__(
+        self,
+        color_theme,
+        title,
+        logo_files,
+        logo_width,
+        logo_height,
+        tabs,
+        pad,
+    ):
         super().__init__()
 
         ctk.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
         # sets all colors and default font:
-        ctk.set_default_color_theme("assets/rsna_color_scheme_font.json")
+        ctk.set_default_color_theme(color_theme)
 
         self.geometry("800x600")
         self.font = ctk.CTkFont()  # get default font as defined in json file
@@ -45,8 +67,8 @@ class App(ctk.CTk):
 
         # Logo:
         self.logo = ctk.CTkImage(
-            light_image=Image.open(logo_file),
-            dark_image=Image.open(logo_file),
+            light_image=Image.open(logo_files[0]),
+            dark_image=Image.open(logo_files[1]),
             size=(logo_width, logo_height),
         )
         self.logo = ctk.CTkLabel(self, image=self.logo, text="")
@@ -86,9 +108,11 @@ tabs = {
     "Admin": ["Import Log", "Export Log"],
 }
 
+
 app = App(
+    color_theme="assets/rsna_color_scheme_font.json",
     title="DICOM Anonymizer Version 17",
-    logo_file="assets/rsna_logo.png",
+    logo_files=("assets/rsna_logo_light.png", "assets/rsna_logo_dark.png"),
     logo_width=75,
     logo_height=20,
     tabs=tabs,
