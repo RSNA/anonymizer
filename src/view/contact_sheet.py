91d170e0ac7d8ab46bc48564d547a56b942b0379
mdevans <michael@dx.life>
2024-11-12 15:26:23 +0200

    retry signing

--- src/view/contact_sheet.py
+++ src/view/contact_sheet.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import logging
+from dataclasses import dataclass
 from pydicom import dcmread
 from pydicom.pixel_data_handlers.util import apply_voi_lut, apply_modality_lut
 import numpy as np
@@ -13,10 +14,19 @@ from utils.translate import _
 logger = logging.getLogger(__name__)
 
 
-class ContactSheet(ctk.CTkToplevel):
-    THUMBNAIL_SIZE = (150, 150)  # width x height pixels
+@dataclass
+class kaleidoscope:
+    patient_id: str
+    study_uid: str
+    series_uid: str
+    series_description: str
+    thumbnails: list[Image.Image]
 
-    def __init__(self, patient_ids, base_dir):
+
+class ContactSheet(ctk.CTkToplevel):
+    THUMBNAIL_SIZE = (200, 200)  # width x height pixels
+
+    def __init__(self, patient_ids: list[str], base_dir: Path):
         super().__init__()
         self._patient_ids = patient_ids
         self._base_dir = base_dir
@@ -28,10 +38,11 @@ class ContactSheet(ctk.CTkToplevel):
         self._frame.pack(fill="both", expand=True)
         self._columns = max(800 // self.THUMBNAIL_SIZE[0], 1)
         logger.info(f"ContactSheet for Patients={len(patient_ids)}, Columns={self._columns}")
-        self._load_kaleidoscopes(patient_ids)
+        self._create_kaleidoscopes(patient_ids)
+        self._create_sheet()
 
-    def _on_image_click(self, event, dcm_path, frame_number):
-        logger.info(f"Image clicked: {dcm_path}, Frame: {frame_number}")
+    def _on_image_click(self, event, kaleidoscope):
+        logger.info(f"Image clicked: kaleidoscope: {kaleidoscope}")
 
     def _load_patient_image_frames(self, patient_ids):
 
@@ -88,92 +99,104 @@ class ContactSheet(ctk.CTkToplevel):
                     lambda event, path=dcm_path, frame_number=frame: self._on_image_click(event, path, frame_number),
                 )
 
-    def _load_kaleidoscopes(self, patient_ids):
+    def _create_kaleidoscopes(self):
 
-        total_thumbnails = 0
+        self._kaleidoscopes = []
 
-        for patient_id in patient_ids:
+        for patient_id in self._patient_ids:
 
-            patient_path = Path(self._base_dir / patient_id)
+            patient_path = self._base_dir / Path(patient_id)
 
             for study_path in patient_path.iterdir():
-                if study_path.is_dir():
-                    for series_path in study_path.iterdir():
-                        if series_path.is_dir():
-                            dcm_paths = sorted(get_dcm_files(series_path))
 
-                        logger.info(f"Processing series in {series_path} with {len(dcm_paths)} DICOM file(s)")
+                if not study_path.is_dir():
+                    continue
 
-                        all_pixels = []
+                for series_path in study_path.iterdir():
 
-                        for dcm_path in dcm_paths:
-                            ds = dcmread(dcm_path)
-                            pixels = ds.pixel_array
-                            pi = ds.get("PhotometricInterpretation", None)
-                            if pi is None:
-                                continue
+                    if not series_path.is_dir():
+                        continue
 
-                            if pi in ["MONOCHROME1", "MONOCHROME2"]:
-                                pixels = apply_voi_lut(apply_modality_lut(pixels, ds), ds)
-                                normalize(
-                                    src=pixels,
-                                    dst=pixels,
-                                    alpha=0,
-                                    beta=255,
-                                    norm_type=NORM_MINMAX,
-                                    dtype=CV_8U,
-                                    mask=None,
-                                )
-                                pixels = pixels.astype(np.uint8)
-                                if pi == "MONOCHROME1":
-                                    pixels = np.invert(pixels)
+                    dcm_paths = sorted(get_dcm_files(series_path))
 
-                            no_of_frames = ds.get("NumberOfFrames", 1)
-                            if no_of_frames == 1:
-                                all_pixels.append(pixels[..., np.newaxis] if pixels.ndim == 2 else pixels)
-                            else:
-                                for frame in range(no_of_frames):
-                                    all_pixels.append(
-                                        pixels[frame][..., np.newaxis] if pixels.ndim == 2 else pixels[frame]
-                                    )
+                    logger.info(f"Processing series in {series_path} with {len(dcm_paths)} DICOM file(s)")
 
-                            # Create low, mid, and high contrast images
-                            if all_pixels:
-                                combined_image = np.mean(all_pixels, axis=0).astype(np.uint8)
-                                low_contrast = np.clip(combined_image * 0.5, 0, 255).astype(np.uint8)
-                                mid_contrast = combined_image
-                                high_contrast = np.clip(combined_image * 1.5, 0, 255).astype(np.uint8)
+                    cumulative_pixels = []
+                    frame_count = 0
 
-                                # Create a rotating thumbnail
-                                images = [low_contrast, mid_contrast, high_contrast]
-                                thumbnails = [
-                                    Image.fromarray(img)
-                                    .convert("RGB")
-                                    .resize(tuple(dim * 2 for dim in self.THUMBNAIL_SIZE), Image.Resampling.NEAREST)
-                                    for img in images
-                                ]
+                    for dcm_path in dcm_paths:
+                        ds = dcmread(dcm_path)
+                        pixels = ds.pixel_array
+                        pi = ds.get("PhotometricInterpretation", None)
+                        if pi is None:
+                            continue
 
-                                # Create and display CTkLabel that rotates images every 0.5 seconds
-                                ctk_image = ctk.CTkImage(light_image=thumbnails[0], size=self.THUMBNAIL_SIZE)
-                                label = ctk.CTkLabel(self._frame, image=ctk_image, text="")
-                                row = int(total_thumbnails // self._columns)
-                                col = int(total_thumbnails % self._columns)
-                                label.grid(row=row, column=col, padx=1, pady=1, sticky="nsew")
-                                total_thumbnails += 1
+                        if pi in ["MONOCHROME1", "MONOCHROME2"]:
+                            pixels = apply_voi_lut(apply_modality_lut(pixels, ds), ds)
+                            normalize(
+                                src=pixels,
+                                dst=pixels,
+                                alpha=0,
+                                beta=255,
+                                norm_type=NORM_MINMAX,
+                                dtype=CV_8U,
+                                mask=None,
+                            )
+                            pixels = pixels.astype(np.uint8)
+                            if pi == "MONOCHROME1":
+                                pixels = np.invert(pixels)
 
-                                # Add rotation logic
-                                def rotate_images():
-                                    index = 0
-                                    while True:
-                                        ctk_image.configure(light_image=thumbnails[index])
-                                        self._frame.update_idletasks()
-                                        self._frame.after(500)  # Wait for 0.5 seconds
-                                        index = (index + 1) % len(thumbnails)
+                        no_of_frames = ds.get("NumberOfFrames", 1)
+                        if no_of_frames == 1:
+                            cumulative_pixels += pixels if pixels.ndim == 2 else np.sum(pixels, axis=0)
+                            frame_count += 1
+                        else:
+                            for frame in range(no_of_frames):
+                                frame_data = pixels[frame] if pixels.ndim == 3 else pixels[..., frame]
+                                cumulative_pixels += frame_data
+                                frame_count += 1
 
-                                label.bind(
-                                    "<Button-1>",
-                                    lambda event, path=series_path, frame_number=0: self._on_image_click(
-                                        event, path, frame_number
-                                    ),
-                                )
-                                self._frame.after(0, rotate_images)
+                        mean_pixels = (cumulative_pixels / frame_count).astype(np.uint8)
+                        low_contrast = np.clip(mean_pixels * 0.5, 0, 255).astype(np.uint8)
+                        high_contrast = np.clip(mean_pixels * 1.5, 0, 255).astype(np.uint8)
+
+                        thumbnails = [
+                            Image.fromarray(img)
+                            .convert("RGB")
+                            .resize(tuple(dim * 2 for dim in self.THUMBNAIL_SIZE), Image.Resampling.NEAREST)
+                            for img in [low_contrast, mean_pixels, high_contrast]
+                        ]
+
+                        self._kaleidoscopes.append(
+                            kaleidoscope(
+                                patient_id=patient_id,
+                                study_uid=ds.StudyInstanceUID,
+                                series_uid=ds.SeriesInstanceUID,
+                                series_description=ds.get("SeriesDescription", "?"),
+                                thumbnails=thumbnails,
+                            )
+                        )
+
+    def _create_sheet(self):
+        total_thumbnails = 0
+
+        for kaleidoscope in self._kaleidoscopes:
+            ctk_image = ctk.CTkImage(light_image=kaleidoscope.thumbnails[1], size=self.THUMBNAIL_SIZE)
+            label = ctk.CTkLabel(self._frame, image=ctk_image, text="")
+            row = int(total_thumbnails // self._columns)
+            col = int(total_thumbnails % self._columns)
+            label.grid(row=row, column=col, padx=1, pady=1, sticky="nsew")
+            total_thumbnails += 1
+            label.bind(
+                "<Button-1>",
+                lambda event, kaleidoscope=kaleidoscope: self._on_image_click(event, kaleidoscope),
+            )
+
+        # # Add rotation logic
+        # def rotate_images():
+        #     index = 0
+        #     while True:
+        #         ctk_image.configure(light_image=thumbnails[index])
+        #         self._frame.update_idletasks()
+        #         self._frame.after(500)  # Wait for 0.5 seconds
+        #         index = (index + 1) % len(thumbnails)
