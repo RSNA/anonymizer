b383ab79dc5391aa3e60b588e2ac9847a59f3338
mdevans <michael@dx.life>
2025-01-23 13:17:49 +0200

    create poetry script for package execution

--- src/anonymizer/utils/version.py
+++ src/anonymizer/utils/version.py
@@ -2,6 +2,17 @@ import importlib.metadata
 import toml
 from pathlib import Path
 
+# poetry version [command]:
+
+# patch: Increments the patch version.
+# Example: 1.0.0 to 1.0.1.
+
+# minor: Increments the minor version.
+# Example: 1.0.0 to 1.1.0.
+
+# major: Increments the major version.
+# Example: 1.0.0 to 2.0.0.
+
 
 def get_version() -> str:
     try:
